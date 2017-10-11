04769a89c341d5ab95198fa44a696075ad3dafd4
Steven Portley <portleys4@students.rowan.edu>
2017-10-11 13:14:49 -0400

    checkout

--- src/server.js
+++ src/server.js
@@ -1,39 +1,73 @@
 var equipment_sheet = SpreadsheetApp.openById("13RX9kDpH1E1fhoUhit8bFBJh-5wQ3R6_S0j1NbqQc-o");
 //var equipment_sheet = SpreadsheetApp.openById("1tV_IAl5obBcWzMqI6Q1ZUdDcRWX0JADSSaIh2SzNqi8");
 
+var values = equipment_sheet.getDataRange().getValues();
 
-function getItem(item_name) {
+
+
+
+
+
+function getItemInfo(instrument_id) {
   
-  var values = equipment_sheet.getDataRange().getValues();
   
   var item = {
-    itemName: null,
-    itemLoc: null,
-    itemBorrower: null
+    id: null,
+    manf: null,
+    partno: null,
+    serialno: null,
+    type: null,
+    curr_loc: null,
+    home_loc: null,
+    borr_email: null
   
   }
   
   
-  for(var i = 0; i < values.length; i++){
-    if(values[i][0] == item_name){
-      item.itemName = values[i][1];
-      item.prop2 = values[i][2];
-      item.prop3 = values[i][3];
+  for(var i = 1; i < values.length; i++){
+    if(values[i][0] == instrument_id){
+      item.id = values[i][1];
+      item.manf = values[i][2];
+      item.partno = values[i][3];
+      item.serialno = values[i][4];
+      item.type = values[i][5];
+      item.curr_loc = values[i][6];
+      item.home_loc = values[i][7];
+      item.borr_email = values[i][8];
+      
       break;
     }
   }
   
-  return item;
+  return item 
   
 
 }
 
 
-function addItem(item){
+function checkOut(instrument_id, new_loc, email){
 
-  equipment_sheet.appendRow( [item.itemName, item.itemLoc, item.Borrower]  );
-  
-  
+  for(var i = 1; i < values.length; i++){
+    if(values[i][0] == instrument_id){
+        var email_cell = "H" + i.toString(); 
+        var new_loc_cell = "F" + i.toString();
+
+        email_cell = equipment_sheet.getRange(email_cell);
+        new_loc_cell = equipment_sheet.getRange(new_loc_cell);
+
+        email_cell.setValue(email);
+        new_loc_cell.setValue(new_loc);
+         
+      break;
+    }
+
+
+
+}
+
+
+function addItem(item){
+    equipment_sheet.appendRow( [item.id, item.manf, item.partno, item.serialno, item.type, item.curr_loc, item.home_loc, item.borr_email] );
 }
 
 
